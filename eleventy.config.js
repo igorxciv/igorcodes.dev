26efb4f0ffc10894957fc364289124927823336f
igorxciv <icheliadinski@gmail.com>
2023-12-03 19:02:13 -0700

    feat: update styles

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,43 @@
 const yaml = require('js-yaml');
+const lightningcss = require('lightningcss');
+const packageJson = require('./package.json');
 
 module.exports = (config) => {
 	// YAML
+	config.addDataExtension('yml', (contents) => yaml.load(contents));
 
-	config.addDataExtension('yml', (contents) => {
-		return yaml.load(contents);
+	// CSS
+	const styles = [
+		'./src/styles/index.css',
+		'./src/styles/light.css',
+		'./src/styles/dark.css',
+	];
+
+	const processStyles = (path) => lightningcss.bundle({
+		filename: path,
+		minify: true,
+		sourceMap: false,
+		targets: lightningcss.browserslistToTargets(packageJson.browserslist),
+		include: lightningcss.Features.MediaQueries | lightningcss.Features.Nesting
+	});
+
+	config.addTemplateFormats('css');
+
+	config.addExtension('css', {
+		outputFileExtension: 'css',
+		compile: async (content, path) => {
+			if (!styles.includes(path)) return;
+
+			return async () => {
+				const { code } = await processStyles(path);
+				return code;
+			}
+		}
+	});
+
+	config.addFilter('css', async (path) => {
+		const { code } = await processStyles(path);
+		return code;
 	});
 
 	// Passthrough copy
