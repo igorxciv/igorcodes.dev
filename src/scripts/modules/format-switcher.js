50576c964719bc873b0fe11c7166f6a59dc6255b
igorxciv <icheliadinski@gmail.com>
2023-12-20 21:21:32 -0700

    feat: add local storage save

--- src/scripts/modules/format-switcher.js
+++ src/scripts/modules/format-switcher.js
@@ -2,6 +2,13 @@ const formatSwitcher = document.querySelector('.format-switcher');
 const formatButtons = document.querySelectorAll('.format-switcher__button');
 
 function setupSwitcher() {
+	const savedFormat = getSavedFormat();
+
+	if (savedFormat !== null) {
+		const currentButton = formatSwitcher.querySelector(`[value=${savedFormat}]`);
+		pressButton(currentButton, true);
+	}
+
 	formatSwitcher.addEventListener('click', (event) => {
 		const isButton = event.target.tagName === 'BUTTON';
 		const isPressed = event.target.getAttribute('aria-pressed') === 'true';
@@ -9,6 +16,7 @@ function setupSwitcher() {
 		if (!isButton || isPressed) return;
 
 		pressButton(event.target, true);
+		setFormat(event.target.value);
 	})
 }
 
@@ -17,6 +25,14 @@ function pressButton(button, press) {
 		button.setAttribute('aria-pressed', !press);
 	}
 	button.setAttribute('aria-pressed', press);
+}
+
+function getSavedFormat() {
+	return localStorage.getItem('experience-format');
+}
+
+function setFormat(format) {
+	localStorage.setItem('experience-format', format);
 }
 
 setupSwitcher();
