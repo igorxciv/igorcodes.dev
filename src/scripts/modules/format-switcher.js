18a79c668412337eda20753582d1031968891d99
igorxciv <icheliadinski@gmail.com>
2023-12-26 22:04:43 -0700

    feat: update codestyle

--- src/scripts/modules/format-switcher.js
+++ src/scripts/modules/format-switcher.js
@@ -1,69 +1,71 @@
-const formatSwitcher = document.querySelector('.format-switcher')
-const formatButtons = document.querySelectorAll('.format-switcher__button')
-const lyricalExperience = document.querySelectorAll('.experience__lyrical')
-const factsExperience = document.querySelectorAll('.experience__facts')
+const formatSwitcher = document.querySelector('.format-switcher');
+const formatButtons = document.querySelectorAll('.format-switcher__button');
+const lyricalExperience = document.querySelectorAll('.experience__lyrical');
+const factsExperience = document.querySelectorAll('.experience__facts');
 
 function setupSwitcher() {
-  const savedFormat = getSavedFormat()
+  const savedFormat = getSavedFormat();
 
   if (savedFormat !== null) {
-    const currentButton = formatSwitcher.querySelector(`[value=${savedFormat}]`)
-    pressButton(currentButton, true)
-    setExperienceFormat(savedFormat)
+    const currentButton = formatSwitcher.querySelector(
+      `[value=${savedFormat}]`,
+    );
+    pressButton(currentButton, true);
+    setExperienceFormat(savedFormat);
   }
 
   formatSwitcher.addEventListener('click', event => {
-    const isButton = event.target.tagName === 'BUTTON'
-    const isPressed = event.target.getAttribute('aria-pressed') === 'true'
+    const isButton = event.target.tagName === 'BUTTON';
+    const isPressed = event.target.getAttribute('aria-pressed') === 'true';
 
-    if (!isButton || isPressed) return
+    if (!isButton || isPressed) return;
 
-    pressButton(event.target, true)
-    setExperienceFormat(event.target.value)
-    setFormat(event.target.value)
-  })
+    pressButton(event.target, true);
+    setExperienceFormat(event.target.value);
+    setFormat(event.target.value);
+  });
 }
 
 function setupFormat() {
-  const savedFormat = getSavedFormat()
+  const savedFormat = getSavedFormat();
 
-  if (savedFormat === null) return
+  if (savedFormat === null) return;
 
-  setFormat(savedFormat)
+  setFormat(savedFormat);
 }
 
 function pressButton(button, press) {
   formatButtons.forEach(btn => {
-    btn.setAttribute('aria-pressed', !press)
-  })
-  button.setAttribute('aria-pressed', press)
+    btn.setAttribute('aria-pressed', !press);
+  });
+  button.setAttribute('aria-pressed', press);
 }
 
 function setExperienceFormat(format) {
   if (format === 'lyrically') {
     lyricalExperience.forEach(item => {
-      item.classList.remove('hidden')
-    })
+      item.classList.remove('hidden');
+    });
     factsExperience.forEach(item => {
-      item.classList.add('hidden')
-    })
+      item.classList.add('hidden');
+    });
   } else {
     lyricalExperience.forEach(item => {
-      item.classList.add('hidden')
-    })
+      item.classList.add('hidden');
+    });
     factsExperience.forEach(item => {
-      item.classList.remove('hidden')
-    })
+      item.classList.remove('hidden');
+    });
   }
 }
 
 function getSavedFormat() {
-  return localStorage.getItem('experience-format')
+  return localStorage.getItem('experience-format');
 }
 
 function setFormat(format) {
-  localStorage.setItem('experience-format', format)
+  localStorage.setItem('experience-format', format);
 }
 
-setupSwitcher()
-setupFormat()
+setupSwitcher();
+setupFormat();
